72b82e58cb5ad7b1cc8ff6ba145e1fb2ecebe332
Joper322228 <mykola.puzdria@britenet.com.pl>
2022-07-26 12:02:30 +0200

    add milestone trigger

--- force-app/main/default/lwc/orderHistory/orderHistory.js
+++ force-app/main/default/lwc/orderHistory/orderHistory.js
@@ -1,10 +1,12 @@
 import { LightningElement, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getUserOrders from '@salesforce/apex/CS_OrderHistoryController.getUserOrders';
+import NO_ORDER from '@salesforce/resourceUrl/No_Order';
 
 export default class OrderHistory extends LightningElement {
     @track userOrders = [];
     isEmpty = false;
+    noOrder = NO_ORDER;
 
     @wire(getUserOrders, {})
     getUserOrders({ error, data }) {
